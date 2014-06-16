3bbfcfcef31e0aba7207d999259af96eab3fb307
Maria Vidal <mariavp567@yahoo.es>
2014-06-16 22:54:12 +0200

    application.rb test

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Tfg
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ca
-    #config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "fontawesome", "fonts", "fontawesome-webfont.woff")
+    config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "fontawesome", "fonts", "fontawesome-webfont.woff")
   end
 end
