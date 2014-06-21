324b20217ffdaa30cf0c4176d234de9d7cd151ae
Maria Vidal <mariavp567@yahoo.es>
2014-06-21 09:42:00 +0200

    configurant rspec i setting up primers test a usuari

---
+++ spec/controllers/usuaris_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe UsuarisController, :type => :controller do
+
+end
---
+++ spec/helpers/usuaris_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the TestaaaHelper. For example:
+#
+# describe TestaaaHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe UsuarisHelper, :type => :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
